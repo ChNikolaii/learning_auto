6b81e3db99a5401cbfc85ce64676be93f88fb05f
Николай Черентаев <n.cherentaev@quality-lab.ru>
2021-09-26 14:31:10 +0300

    Создал класс Point, создал функцию, сделал запускаемый класс

---
+++ sandbox/src/main/java/RunPoint.java
@@ -0,0 +1,10 @@
+public class RunPoint {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(9,4);
+        Point p2 = new Point(10,10);
+        System.out.println("Растояние между точками P1 и P2:"+" " +p1.distance(p2));
+
+    }
+
+}
