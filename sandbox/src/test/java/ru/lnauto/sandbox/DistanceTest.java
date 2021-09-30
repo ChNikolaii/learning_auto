91beda40a83da1022096e7b6f25c48350ee7c06f
Николай Черентаев <n.cherentaev@quality-lab.ru>
2021-09-30 18:27:33 +0300

    Исправил "Первый тест с Testng" (Задание №3)

--- sandbox/src/test/java/ru/lnauto/sandbox/DistanceTest.java
+++ sandbox/src/test/java/ru/lnauto/sandbox/DistanceTest.java
@@ -9,8 +9,8 @@ public class DistanceTest {
 
     @Test
 public void testDistance() {
-    Point p1 = new Point(5, 2);
-    Point p2 = new Point(6,3);
-        Assert.assertEquals(p1.distance(5,2), p2.distance(6,3));
+    Point p1 = new Point(9, 5);
+    Point p2 = new Point(20,20);
+        Assert.assertEquals(p1.distance(p2), 18.601075237738275);
     }
 }
