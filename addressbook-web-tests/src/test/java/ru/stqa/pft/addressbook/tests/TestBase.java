240f4e28dd544c276774378d8640ce60d14aebe6
Николай Черентаев <n.cherentaev@quality-lab.ru>
2021-10-31 15:05:46 +0300

    Вынеc описание тестового стенда в конфигурационный файл

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -5,13 +5,17 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.io.IOException;
+
+import static org.openqa.selenium.remote.BrowserType.*;
+
 public class TestBase {
 
     protected final ApplicationManager app
-            = new ApplicationManager(System.getProperty("browser"));
+            = new ApplicationManager(System.getProperty("browser", CHROME));
 
     @BeforeMethod(alwaysRun = true)
-    public void setUp() {
+    public void setUp() throws IOException {
         app.init();
     }
 
