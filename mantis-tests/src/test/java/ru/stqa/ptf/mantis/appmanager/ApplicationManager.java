20aadc3da922fe12e68f6df8a1264d7b7640a9ba
Николай Черентаев <n.cherentaev@quality-lab.ru>
2021-11-14 04:19:46 +0300

    ошибка на тесте RegistrationTest

--- mantis-tests/src/test/java/ru/stqa/ptf/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/ptf/mantis/appmanager/ApplicationManager.java
@@ -1,7 +1,5 @@
 package ru.stqa.ptf.mantis.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -16,9 +14,12 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
     private final Properties properties;
-    WebDriver wd;
+    private WebDriver wd;
 
     private String browser;
+    private RegistrationHelper registrationHelper;
+    private FtpHelper ftp;
+    private MailHelper mailHelper;
 
 
     public ApplicationManager(String browser) {
@@ -31,25 +32,56 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-        if (browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.EDGE)) {
-            wd = new EdgeDriver();
-        }
-        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get(properties.getProperty("web.baseUrl"));
 
-    }
 
-    public void logOut() {
-        wd.findElement(By.linkText("Logout")).click();
     }
 
     public void stop() {
-        logOut();
-        wd.quit();
+        if (wd != null) {
+            wd.quit();
+        }
+
     }
 
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+    public FtpHelper ftp() {
+        if (ftp == null) {
+            ftp = new FtpHelper(this);
+        }
+        return ftp;
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.EDGE)) {
+                wd = new EdgeDriver();
+            }
+            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
+    }
+    public MailHelper mail() {
+        if (mailHelper == null) {
+            mailHelper = new MailHelper(this);
+        }
+        return mailHelper;
+    }
 }
--- mantis-tests/src/test/java/ru/stqa/ptf/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/ptf/mantis/tests/TestBase.java
@@ -4,6 +4,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.ptf.mantis.appmanager.ApplicationManager;
 
+import java.io.File;
 import java.io.IOException;
 
 import static org.openqa.selenium.remote.BrowserType.CHROME;
@@ -16,10 +17,13 @@ public class TestBase {
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws IOException {
         app.init();
+        app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak");
     }
 
+
     @AfterMethod(alwaysRun = true)
-    public void tearDown() {
+    public void tearDown() throws IOException {
+        app.ftp().restore("config_inc.php.bak", "config_inc.php");
         app.stop();
     }
 
